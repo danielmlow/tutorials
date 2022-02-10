4517607f3ac63000b771448df45a2438c7fc931c
danielmlow <danielmlow@>
2022-02-10 10:27:51 -0500

    CLI for annotation, readme for convert mp3

--- annotation.py
+++ annotation.py
@@ -1,7 +1,7 @@
 """
 python3
 IMPORTANT. Install:
-pip3 install playsound PyObjC pandas 
+pip3 install playsound PyObjC pandas pyaudio
 """
 
 import os 
@@ -13,17 +13,27 @@ from playsound import playsound
 import pyaudio
 import wave
 from scipy.io import wavfile
+import argparse
 
-# config
-data_dir = 'vfp_audios_16khz' #used to name the output file as well
-input_dir = f'./data/input/{data_dir}/'
-output_dir = './data/output/annotations/'
-instructions = '1=if volume is very low; 2=normal; 3=gain was likely raised; 99=unsure; r=repeat; q=save and quit. Or instead type in a note here: '
-play_n_seconds = 5
+# Create the parser
+parser = argparse.ArgumentParser(description='Annotate files in input_dir according to your instructios argument and output a csv')
+# Add an argument
+parser.add_argument('--input_dir', type=str, required=True, help = 'for instance: data/input/audios_16khz/')
+parser.add_argument('--output_dir', type=str, required=True, help = 'will create if doesnt exist, for instance: data/output/annotations/')
+parser.add_argument('--filter', type=str, default=None, help = 'keep files that include this string, for instance: Speech_1')
+parser.add_argument('--instructions', type=str,default='1=if volume is very low; 2=normal; 3=gain was likely raised; 99=unsure; r=repeat; q=save and quit. Or instead type in a note here:', help='default is: 1=if volume is very low; 2=normal; 3=gain was likely raised; 99=unsure; r=repeat; q=save and quit. Or instead type in a note here:')
+parser.add_argument('--play_n_seconds', type=int,default=5, help='int to pay int sec or 0 to play full, play first n seconds in case files are long. Default is 5.')
 
-# main
-# ===========================
 
+# Parse the argument
+args = parser.parse_args()
+input_dir = args.input_dir
+output_dir = args.output_dir
+filter = args.filter
+print(filter)
+print(type(filter))
+instructions = args.instructions
+play_n_seconds = args.play_n_seconds
 
 
 def play_partial_sound(path_to_wav, start = 0, play_n_seconds = play_n_seconds):
@@ -53,38 +63,62 @@ def play_partial_sound(path_to_wav, start = 0, play_n_seconds = play_n_seconds):
   wave_file.close()
   return
 
+if __name__ == '__main__':
+    
+    try:
+        os.mkdir(output_dir)
+    except:
+        pass
+    
+    files = os.listdir(input_dir)
+    try: 
+            files.remove('.DS_Store')
+    except:
+        pass
+    if filter:
+      files = [n for n in files if filter in n] #just keep first sample of Speech for each participant
+    print(len(files), 'files')
+    # print('avg. length 6.8 sec +- 5.4 sec')
+    print(instructions+'\n')
 
+    seed = 123
+    random.Random(seed).shuffle(files) ## shuffle order of files with seed so that if you quit in the middle, you can continue where you left off
+    
+    annotation = []
+    lengths = []
+    for i, file_i in enumerate(files):
+      # get length
+      samplerate, data = wavfile.read(input_dir+file_i) 
+      length = np.round(data.shape[0]/samplerate,1)
+      if play_n_seconds: 
+        # play partial
+        print(f'======= file #{i}, {length} sec, playing first {play_n_seconds} sec ...')
+        play_partial_sound(input_dir+file_i, start = 0, play_n_seconds = play_n_seconds)
+      else:
+        # play full
+        print(f'======= playing file #{i}, {length} sec ...')
+        playsound(input_dir+file_i) #play audio
+      resp = input(instructions) #record annotation
+      if resp == 'r':
+        if play_n_seconds: 
+          # play partial
+          print(f'======= file #{i}, {length} sec, playing first {play_n_seconds} sec ...')
+          play_partial_sound(input_dir+file_i, start = 0, play_n_seconds = play_n_seconds)
+        else:
+          # play full
+          print(f'======= playing file #{i}, {length} sec ...')
+          playsound(input_dir+file_i) #play audio
+        
 
-files = os.listdir(input_dir)
-files = [n for n in files if 'Speech_1' in n] #just keep first sample of Speech for each participant
-print(len(files), 'files')
-print('avg. length 6.8 sec +- 5.4 sec')
-print(instructions+'\n')
+      elif resp == 'q':
+        break
+      else:
+        annotation.append([file_i, resp, ])
+      print('\n')
 
-seed = 123
-random.Random(seed).shuffle(files) ## shuffle order of files with seed so that if you quit in the middle, you can continue where you left off
-annotation = []
-lengths = []
-for i, file_i in enumerate(files):
-  # get length
-  samplerate, data = wavfile.read(input_dir+file_i) 
-  length = np.round(data.shape[0]/samplerate,1)
-  print(f'======= playing file #{i}, {length} sec ...')
-  play_partial_sound(input_dir+file_i, start = 0, play_n_seconds = play_n_seconds)
-  # playsound(input_dir+file_i) #play audio
-  resp = input(instructions) #record annotation
-  if resp == 'r':
-    play_partial_sound(input_dir+file_i, start = 0, play_n_seconds = play_n_seconds)
-    # playsound(input_dir+file_i)
-    resp = input(instructions)
-  elif resp == 'q':
-    break
-  else:
-    annotation.append([file_i, resp, ])
-  print('\n')
-
-# create DF
-annotation = pd.DataFrame(annotation, columns = ['file', 'annotation'])
-ts = datetime.datetime.utcnow().strftime('%y-%m-%dT%H-%M-%S')
-annotation.to_csv(output_dir+f'annotations_{data_dir}_{ts}.csv')
+    # create DF
+    annotation = pd.DataFrame(annotation, columns = ['file', 'annotation'])
+    ts = datetime.datetime.utcnow().strftime('%y-%m-%dT%H-%M-%S')
+    output_filename = input_dir.split('/')[-2] 
+    annotation.to_csv(output_dir+f'annotations_{output_filename}_{ts}.csv')
 
